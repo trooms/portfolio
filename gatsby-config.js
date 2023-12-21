6b1dae684558d227c96d451930462582ac3f7b5c
trooms <tcostell@usc.edu>
2023-12-20 21:32:23 -0800

    fixed wording

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
     // Main Site Title
     title: `Truman Costello`,
     // Description that goes under your name in main bio
-    description: `Computer Science, Game Development, and Applied & Computational Math @ USC`,
+    description: `Computer Science, Game Development, Mathematics @ USC`,
     // Optional: Github account URL
     github: `https://github.com/trooms`,
     // Content of the About Me section
     about: `I am a third-year bachelors student in Computing Science (Games) and AMCM at the University of Southern California.
             \nI'm currently working on a project focused on bringing applications of machine learning to education.
-            \nApart from school, I take care of my dog Sloane and work as a server at a local off-campus restaurant 'Study Hall'.`,
+            \nApart from school, I take care of my dog Sloane and work as a server at Study Hall, a local off-campus restaurant.`,
     // Optional: List your education, they must have `name` and `description`. `link` is optional.
     projects: [
       {
