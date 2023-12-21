82c1eb8ebb73863debcc963c07d24ee28d3e0037
trooms <tcostell@usc.edu>
2023-12-20 21:23:02 -0800

    remove pattern

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -8,39 +8,9 @@ const classes = {
 
 const Layout = ({ children }) => {
   return (
-    <div className={classes.outerWrapper}>
-      <svg
-        className={classes.svg}
-        width="404"
-        height="700"
-        fill="none"
-        viewBox="0 0 404 784"
-      >
-        <defs>
-          <pattern
-            id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              className="text-gray-200"
-              fill="#edf2f7"
-            />
-          </pattern>
-        </defs>
-        <rect
-          width="404"
-          height="700"
-          fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
-        />
-      </svg>
+    <div
+      className={classes.outerWrapper}
+    >
       <div className={classes.wrapper}>{children}</div>
     </div>
   );
