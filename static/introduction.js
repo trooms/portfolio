78e64bac5a499f96596dfe423f053cb065e9ddce
trooms <tcostell@usc.edu>
2024-02-26 09:55:14 -0800

    add anim

--- static/introduction.js
+++ static/introduction.js
@@ -1,44 +1,34 @@
 console.clear();
-console.log("Building introduction canvas");
 
 // Get the canvas element from the DOM
 const canvas = document.getElementById('introduction');
-
-// Get the canvas dimensions
-let width = canvas.offsetWidth; // Width of the scene
-let height = canvas.offsetHeight; // Height of the scene
-
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
 // Store the 2D context
 const ctx = canvas.getContext('2d');
 
-// Function called right after user resized its screen
-function onResize () {
-  // We need to define the dimensions of the canvas to our canvas element
-  // Javascript doesn't know the computed dimensions from CSS so we need to do it manually
-  width = canvas.offsetWidth;
-  height = canvas.offsetHeight;
-  
-  // If the screen device has a pixel ratio over 1
-  // We render the canvas twice bigger to make it sharper (e.g. Retina iPhone)
-  if (window.devicePixelRatio > 1) {
-    canvas.width = canvas.clientWidth * 2;
-    canvas.height = canvas.clientHeight * 2;
-    ctx.scale(2, 2);
-  } else {
-    canvas.width = width;
-    canvas.height = height;
-  }
+if (window.devicePixelRatio > 1) {
+  canvas.width = canvas.clientWidth * 2;
+  canvas.height = canvas.clientHeight * 2;
+  ctx.scale(2, 2);
 }
 
-// Listen to resize events
-window.addEventListener('resize', onResize);
-// Make sure the canvas size is perfect
-onResize();
+/* ====================== */
+/* ====== VARIABLES ===== */
+/* ====================== */
+let width = canvas.offsetWidth; // Width of the canvas
+let height = canvas.offsetHeight; // Height of the canvas
+const dots = []; // Every dots in an array
 
-let PERSPECTIVE = width * 0.8; // The field of view of our 3D scene
-let PROJECTION_CENTER_X = width / 2; // x center of the canvas
-let PROJECTION_CENTER_Y = height / 2; // y center of the canvas
-const dots = []; // Store every particle in this array
+/* ====================== */
+/* ====== CONSTANTS ===== */
+/* ====================== */
+/* Some of those constants may change if the user resizes their screen but I still strongly believe they belong to the Constants part of the variables */
+const DOTS_AMOUNT = 1000; // Amount of dots on the screen
+const DOT_RADIUS = 10; // Radius of the dots
+let PROJECTION_CENTER_X = width / 2; // X center of the canvas HTML
+let PROJECTION_CENTER_Y = height / 2; // Y center of the canvas HTML
+let PERSPECTIVE = width * 0.8;
 
 class Dot {
   constructor() {
@@ -47,46 +37,92 @@ class Dot {
     this.z = Math.random() * width; // Give a random z position
     this.radius = 10; // Size of our element in the 3D world
     
-    this.xProjected = 0; // x coordinate on the 2D world
-    this.yProjected = 0; // y coordinate on the 2D world
-    this.scaleProjected = 0; // Scale of the element on the 2D world (further = smaller)
+    this.xProjected = 0;
+    this.yProjected = 0;
+    this.scaleProjected = 0;
+    
+    TweenMax.to(this, (Math.random() * 10 + 15), {
+      z: width,
+      repeat: -1,
+      yoyo: true,
+      ease: Power2.easeOut,
+      yoyoEase: true,
+      delay: Math.random() * -25
+    });
   }
-  // Project our element from its 3D world to the 2D canvas
+  // Do some math to project the 3D position into the 2D canvas
   project() {
-    // The scaleProjected will store the scale of the element based on its distance from the 'camera'
     this.scaleProjected = PERSPECTIVE / (PERSPECTIVE + this.z);
-    // The xProjected is the x position on the 2D world
     this.xProjected = (this.x * this.scaleProjected) + PROJECTION_CENTER_X;
-    // The yProjected is the y position on the 2D world
     this.yProjected = (this.y * this.scaleProjected) + PROJECTION_CENTER_Y;
   }
   // Draw the dot on the canvas
   draw() {
-    // We first calculate the projected values of our dot
     this.project();
-    // We define the opacity of our element based on its distance
     ctx.globalAlpha = Math.abs(1 - this.z / width);
-    // We draw a rectangle based on the projected coordinates and scale
     ctx.fillRect(this.xProjected - this.radius, this.yProjected - this.radius, this.radius * 2 * this.scaleProjected, this.radius * 2 * this.scaleProjected);
   }
 }
 
-// Create 800 new dots
-for (let i = 0; i < 800; i++) {
-  // Create a new dot and push it into the array
-  dots.push(new Dot());
+function createDots() {
+  // Empty the array of dots
+  dots.length = 0;
+  
+  // Create a new dot based on the amount needed
+  for (let i = 0; i < DOTS_AMOUNT; i++) {
+    // Create a new dot and push it into the array
+    dots.push(new Dot());
+  }
 }
 
+/* ====================== */
+/* ======== RENDER ====== */
+/* ====================== */
 function render() {
-  // Clear the scene from top left to bottom right
+  // Clear the scene
   ctx.clearRect(0, 0, width, height);
   
   // Loop through the dots array and draw every dot
   for (var i = 0; i < dots.length; i++) {
     dots[i].draw();
   }
+  
+  window.requestAnimationFrame(render);
 }
 
+
+// Function called after the user resized its screen
+function afterResize () {
+  width = canvas.offsetWidth;
+  height = canvas.offsetHeight;
+  if (window.devicePixelRatio > 1) {
+    canvas.width = canvas.clientWidth * 2;
+    canvas.height = canvas.clientHeight * 2;
+    ctx.scale(2, 2);
+  } else {
+    canvas.width = width;
+    canvas.height = height;
+  }
+  PROJECTION_CENTER_X = width / 2;
+  PROJECTION_CENTER_Y = height / 2;
+  PERSPECTIVE = width * 0.8;
   
-  // Request the browser the call render once its ready for a new frame
-  window.requestAnimationFrame(render);
+  createDots(); // Reset all dots
+}
+
+// Variable used to store a timeout when user resized its screen
+let resizeTimeout;
+// Function called right after user resized its screen
+function onResize () {
+  // Clear the timeout variable
+  resizeTimeout = window.clearTimeout(resizeTimeout);
+  // Store a new timeout to avoid calling afterResize for every resize event
+  resizeTimeout = window.setTimeout(afterResize, 500);
+}
+window.addEventListener('resize', onResize);
+
+// Populate the dots array with random dots
+createDots();
+
+// Render the scene
+window.requestAnimationFrame(render);
