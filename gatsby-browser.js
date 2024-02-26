c8bc851522d2ec14c073381c005f554e4fdb5819
trooms <tcostell@usc.edu>
2024-02-26 10:33:14 -0800

    route delay for js loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,28 +1,47 @@
-import React from 'react';
 import './src/css/index.css';
 
+let scriptLoaded = false;
+
 export const onRouteUpdate = ({ location }) => {
-    function loadScript(url) {
-      return new Promise((resolve, reject) => {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = url;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.body.appendChild(script);
-      });
-    }
-  
-    async function loadScriptsSequentially() {
-      if (location.pathname.includes('/lightride/')) {
-        try {
-          await loadScript('/TweenMax.min.js');
-          await loadScript('/introduction.js');
-        } catch (error) {
-          console.error("Script loading failed:", error);
+    function initializeCanvas() {
+        if (!scriptLoaded) {
+            loadScriptsSequentially();
         }
-      }
     }
-  
-    loadScriptsSequentially();
+
+    function loadScript(url) {
+        return new Promise((resolve, reject) => {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = url;
+            script.onload = resolve;
+            script.onerror = reject;
+            document.body.appendChild(script);
+        });
+    }
+
+    async function loadScriptsSequentially() {
+        if (location.pathname.includes('/lightride/')) {
+            try {
+                await loadScript('/TweenMax.min.js');
+                await loadScript('/introduction.js');
+                scriptLoaded = true;
+            } catch (error) {
+                console.error("Script loading failed:", error);
+            }
+        }
+    }
+
+    if (location.pathname.includes('/lightride/')) {
+        initializeCanvas();
+    }
+};
+
+export const onRouteUpdateDelayed = ({ location }) => {
+    if (location.pathname.includes('/lightride/')) {
+        // Additional checks or reinitializations if needed
+        if (!scriptLoaded) {
+            initializeCanvas();
+        }
+    }
 };
