52b6d776a0a94009a9478c7879388c5ae52babc7
trooms <tcostell@usc.edu>
2024-02-26 10:04:30 -0800

    load scripts sequentially

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,26 +1,35 @@
 import React from "react";
 
-export function onRenderBody({ setHeadComponents, setPostBodyComponents }) {
-
+export function onRenderBody({ setHeadComponents }) {
     setHeadComponents([
         <script
             dangerouslySetInnerHTML={{
                 __html: `
                 function loadScript(url) {
-                    var script = document.createElement('script');
-                    script.type = 'text/javascript';
-                    script.src = url;
-                    document.body.appendChild(script);
+                    return new Promise((resolve, reject) => {
+                        var script = document.createElement('script');
+                        script.type = 'text/javascript';
+                        script.src = url;
+                        script.onload = resolve;
+                        script.onerror = reject;
+                        document.body.appendChild(script);
+                    });
                 }
 
-                document.addEventListener('DOMContentLoaded', (event) => {
+                async function loadScriptsSequentially() {
                     if (window.location.href.includes('/lightride/')) {
-                        loadScript('/TweenMax.min.js')
-                        loadScript('/introduction.js');
+                        try {
+                            await loadScript('/TweenMax.min.js');
+                            await loadScript('/introduction.js');
+                        } catch (error) {
+                            console.error("Script loading failed:", error);
+                        }
                     }
-                });
+                }
+
+                document.addEventListener('DOMContentLoaded', loadScriptsSequentially);
                 `
             }}
-        />,
+        />
     ]);
 }
