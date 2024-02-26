07482d26ca4c008c6a832d1e7de5a85c09d62cfb
trooms <tcostell@usc.edu>
2024-02-26 13:48:40 -0800

    bruh

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,37 +3,9 @@ import './src/css/index.css';
 let scriptLoaded = false;
 
 export const onRouteUpdate = ({ location }) => {
-    function initializeCanvas() {
-        if (!scriptLoaded) {
-            loadScriptsSequentially();
-            redrawCanvas();
-        }
-    }
-
-    function loadScript(url) {
-        return new Promise((resolve, reject) => {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = url;
-            script.onload = resolve;
-            script.onerror = reject;
-            document.body.appendChild(script);
-        });
-    }
-
-    async function loadScriptsSequentially() {
-        if (location.pathname.includes('/lightride/')) {
-            try {
-                await loadScript('/TweenMax.min.js');
-                await loadScript('/introduction.js');
-                scriptLoaded = true;
-            } catch (error) {
-                console.error("Script loading failed:", error);
-            }
-        }
-    }
-
     if (location.pathname.includes('/lightride/')) {
-        initializeCanvas();
+        if (typeof window.initCanvas === 'function') {
+            window.initCanvas();
+       }
     }
 };
