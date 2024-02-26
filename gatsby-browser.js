20573351e91c77a2e360c4b80cecc1c770ddff2b
trooms <tcostell@usc.edu>
2024-02-26 10:11:11 -0800

    load scripts on route update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,28 @@
 import './src/css/index.css';
+
+exports.onRouteUpdate = ({ location }) => {
+    function loadScript(url) {
+      return new Promise((resolve, reject) => {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+      });
+    }
+  
+    async function loadScriptsSequentially() {
+      if (location.pathname.includes('/lightride/')) {
+        try {
+          await loadScript('/TweenMax.min.js');
+          await loadScript('/introduction.js');
+        } catch (error) {
+          console.error("Script loading failed:", error);
+        }
+      }
+    }
+  
+    loadScriptsSequentially();
+  };
+  
